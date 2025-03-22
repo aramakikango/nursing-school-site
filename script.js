89b9507c671d695640632f34ae6ef5f05429591d
aramakikango <aramakimaos@gmail.com>
2025-03-23 08:49:00 +0900

    初期バージョンのサイトを追加

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-console.log("看護学校受験サイトへようこそ！");
-// Font Awesome を読み込み
-const link = document.createElement('link');
-link.rel = 'stylesheet';
-link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css';
-document.head.appendChild(link);
+window.addEventListener('DOMContentLoaded', () => {
+    const fadeInElements = document.querySelectorAll('.fade-in');
+    fadeInElements.forEach(element => {
+        element.classList.add('visible');
+    });
+});
