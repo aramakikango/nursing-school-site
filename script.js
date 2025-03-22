b0d7fa416a6b8b7cc4c4bb01ec564d6fd092083e
aramakikango <aramakimaos@gmail.com>
2025-03-22 17:28:24 +0900

    志望動機対策ページの追加 & アニメーションとアイコンの追加

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log("看護学校受験サイトへようこそ！");
+// Font Awesome を読み込み
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css';
+document.head.appendChild(link);
